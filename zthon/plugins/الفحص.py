42e51a9dcb3d7e2d66e6c184294f3e1b99b04c7a
SOURCE ELESYED <139072849+githupsource@users.noreply.github.com>
2023-07-27 20:12:10 +0300

    Update الفحص.py

--- zthon/plugins/الفحص.py
+++ zthon/plugins/الفحص.py
@@ -78,7 +78,7 @@ zed_temp = """{ALIVE_TEXT}
 **{Z_EMOJI} إصـدار البـايثون :** `{pyver}`
 **{Z_EMOJI} الوقـت :** `{uptime}`
 **{Z_EMOJI} المسـتخدم:** {mention}
-**{Z_EMOJI} قنـاة السـورس :** [اضغـط هنـا](https://t.me/Tepthon)"""
+**{Z_EMOJI} قنـاة السـورس :** [اضغـط هنـا](https://t.me/def_Zoka)"""
 
 
 @zedub.zed_cmd(
