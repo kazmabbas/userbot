e15bab110d40b5208e386339eca19aa81923cda0
SOURCE ELESYED <139072849+githupsource@users.noreply.github.com>
2023-08-01 23:24:44 +0300

    Add files via upload

---
+++ zthon/plugins/مطورن.py
@@ -0,0 +1,74 @@
+import random
+import re
+import time
+from platform import python_version
+
+from telethon import version, Button, events
+from telethon.errors.rpcerrorlist import (
+    MediaEmptyError,
+    WebpageCurlFailedError,
+    WebpageMediaEmptyError,
+)
+from telethon.events import CallbackQuery
+
+from HuRe import StartTime, l313l, JEPVERSION
+
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
+from ..helpers.utils import reply_id
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from . import mention
+
+plugin_category = "utils"
+
+@l313l.ar_cmd(
+    pattern="المطور$",
+    command=("المطور", plugin_category),
+    info={
+        "header": "لأظهار مطورين السورس",
+        "usage": [
+            "{tr}المطور",
+        ],
+    },
+)
+async def amireallyalive(event):
+    "A kind of showing bot details"
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
+    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
+    CAT_IMG = " https://graph.org/file/6e4359dc87a11152d5951.jpg "
+    if CAT_IMG:
+        CAT = [x for x in CAT_IMG.split()]
+        A_IMG = list(CAT)
+        PIC = random.choice(A_IMG)
+        cat_caption = f"مطورين ريفز\n"
+        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
+        cat_caption += f"- المطور  : @IC_X_K\n"
+        cat_caption += f"- المطور  : @Q_2_Q_Y\n"
+        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
+        await event.client.send_file(
+            event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
+        )
+
+@l313l.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+async def on_plug_in_callback_query_handler(event):
+    statstext = await catalive(StartTime)
+    await event.answer(statstext, cache_time=0, alert=True)
+
+progs = [6301863282, 6301863282, 6301863282, 5838360982]
+
+@l313l.on(events.NewMessage(incoming=True))
+async def reda(event):
+    if event.reply_to and event.sender_id in progs:
+       reply_msg = await event.get_reply_message()
+       owner_id = reply_msg.from_id.user_id
+       if owner_id == l313l.uid:
+           if event.message.message == "حظر من السورس":
+               await event.reply("حاظر مطوري ، لقد تم حظره من استخدام السورس")
+               addgvar("blockedfrom", "yes")
+           elif event.message.message == "الغاء الحظر من السورس":
+               await event.reply("حاظر مطوري، لقد الغيت الحظر")
+               delgvar("blockedfrom")
