afa9011bdd817047571ee02d368e991f06e047bc
SOURCE ELESYED <139072849+githupsource@users.noreply.github.com>
2023-08-06 22:53:50 +0300

    Add files via upload

--- zthon/plugins/رشق مشاهدات.py
+++ zthon/plugins/رشق مشاهدات.py
@@ -18,17 +18,17 @@ from ..core.managers import edit_or_reply
 
 
 
-@zedub.zed_cmd(pattern="رشق تلي ?(.*)")
+@zedub.zed_cmd(pattern="رشق تفاعل?(.*)")
 async def zilzal(event):
     card = event.pattern_match.group(1)
-    chat = "@RSHQ1000bot"
+    chat = "@tf4lbot"
     reply_id_ = await reply_id(event)
     zed = await edit_or_reply(event, "**جـاري رشـق المنشور انتظـر قليـلًا ... 💡**")
     async with event.client.conversation(chat) as conv:
         try:
             await conv.send_message(card)
         except YouBlockedUserError:
-            await zedub(unblock("RSHQ1000bot"))
+            await zedub(unblock("tf4lbot"))
             await conv.send_message(card)
         await asyncio.sleep(2)
         response = await conv.get_response()
