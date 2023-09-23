aa3dadd3b3ab769a894a8302503b96df35f75e09
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-09-23 23:06:44 +0300

    Update سينزر+كلمتك.py

--- zthon/plugins/سينزر+كلمتك.py
+++ zthon/plugins/سينزر+كلمتك.py
@@ -20,14 +20,14 @@ from ..core.managers import edit_or_reply
 @zedub.zed_cmd(pattern="محو ?(.*)")
 async def zilzal(event):
     card = event.pattern_match.group(1)
-    chat = "@maker_refz_bot"
+    chat = "https://t.me/IC_X_X/6"
     reply_id_ = await reply_id(event)
     zed = await edit_or_reply(event, "**جـاري تنفيد طلبك انتظـر قليـلًا ... 💡**")
     async with event.client.conversation(chat) as conv:
         try:
             await conv.send_message(card)
         except YouBlockedUserError:
-            await zedub(unblock("@maker_refz_bot"))
+            await zedub(unblock("https://t.me/IC_X_X/6"))
             await conv.send_message(card)
         await asyncio.sleep(2)
         response = await conv.get_response()
