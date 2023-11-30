c0e47bced936a606c7fa58cc779c6675678ca8d9
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2023-11-30 17:56:42 +0300

    Update حفظ الذاتيه.py

--- zthon/plugins/حفظ الذاتيه.py
+++ zthon/plugins/حفظ الذاتيه.py
@@ -3,13 +3,14 @@ import shutil
 from asyncio import sleep
 from telethon import events
 
-from zthon import zedub
-from zthon.core.logger import logging
+from . import zedub
+from ..core.logger import logging
 from ..helpers.utils import _format
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from ..sql_helper.echo_sql import addecho, get_all_echos, get_echos, is_echo, remove_all_echos, remove_echo, remove_echos
 
-from zthon.core.logger import logging
+from ..sql_helper.autopost_sql import get_all_post
+from ..core.logger import logging
 from . import BOTLOG, BOTLOG_CHATID
 plugin_category = "الادوات"
 LOGS = logging.getLogger(__name__)
@@ -18,17 +19,19 @@ zedself = True
 POSC = gvarstatus("Z_POSC") or "(مم|ذاتية|ذاتيه|جلب الوقتيه)"
 
 ZelzalSelf_cmd = (
-    "𓆩 [ᯓ 𝗦𝗼𝘂𝗿𝗰𝗲 𝐑𝐄𝐅𝐙 - حفـظ الذاتـيـة ♥️](t.me/def_Zoka) 𓆪\n\n"
+    "𓆩 [ᯓ 𝗦𝗼𝘂𝗿𝗰𝗲 𝐑𝐄𝐅𝐙 - حفـظ الذاتيـه 🧧](t.me/def_Zoka) 𓆪\n\n"
     "**⪼** `.تفعيل الذاتيه`\n"
     "**لـ تفعيـل الحفظ التلقائي للذاتيـه**\n"
-    "**سوف يقوم حسابك بحفظ الذاتيه تلقائياً في حافظة حسابك عندما يرسل لك اي شخص ميديـا ذاتيـه**\n\n\n"
+    "**سوف يقوم حسابك بحفظ الذاتيه تلقائياً في حافظة حسابك عندما يرسل لك اي شخص ميديـا ذاتيـه**\n\n"
     "**⪼** `.تعطيل الذاتيه`\n"
-    "**لـ تعطيـل الحفظ التلقائي للذاتيـه**\n\n\n"
+    "**لـ تعطيـل الحفظ التلقائي للذاتيـه**\n\n"
     "**⪼** `.ذاتيه`\n"
-    "**بالـرد ؏ــلى صـوره ذاتيـه لحفظهـا في حال كان امر الحفظ التلقائي معطـل**\n\n"
-    "\n 𓆩 [𝙎𝙊𝙐𝙍𝘾𝞝 𝐑𝐄𝐅𝐙](t.me/def_Zoka) 𓆪"
+    "**بالـرد ؏ــلى صـوره ذاتيـه لحفظهـا في حال كان امر الحفظ التلقائي معطـل**\n\n\n"
+    "**⪼** `.اعلان`\n"
+    "**الامـر + الوقت بالدقائق + الرسـاله**\n"
+    "**امـر مفيـد لجماعـة التمويـل لـ عمـل إعـلان مـؤقت بالقنـوات**\n\n"
+    "\n 𓆩 [𝗦𝗘𝗡𝗭𝗜𝗥 (>_)](t.me/programmer_senzir) 𓆪"
 )
-
 
 @zedub.zed_cmd(pattern="الذاتيه")
 async def cmd(zelzallll):
@@ -40,26 +43,25 @@ async def oho(event):
         return await event.edit("**- ❝ ⌊بالـرد علـى صورة ذاتيـة التدميـر 𓆰...**")
     zzzzl1l = await event.get_reply_message()
     pic = await zzzzl1l.download_media()
-    await zedub.send_file("me", pic, caption=f"**𓆰تم حفـظ الصـورة الذاتـيـة .. بنجـاح ☑️𓆰**")
+    await zedub.send_file("me", pic, caption=f"**⎉╎تم حفـظ الصـورة الذاتيـه .. بنجـاح ☑️𓆰**")
     await event.delete()
 
 @zedub.zed_cmd(pattern="(تفعيل الذاتيه|تفعيل الذاتية)")
 async def start_datea(event):
     global zedself
     if zedself:
-        return await edit_or_reply(event, "**𓆰حفظ الذاتيـة التلقـائي .. مفعـله مسـبـقًا ☑️**")
+        return await edit_or_reply(event, "**⎉╎حفظ الذاتيـة التلقـائي .. مفعـله مسبقـاً ☑️**")
     zedself = True
-    await edit_or_reply(event, "**𓆰تم تفعيـل حفظ الذاتيـة التلقائـي .. بنجـاح ☑️**")
+    await edit_or_reply(event, "**⎉╎تم تفعيـل حفظ الذاتيـة التلقائـي .. بنجـاح ☑️**")
 
 @zedub.zed_cmd(pattern="(تعطيل الذاتيه|تعطيل الذاتية)")
 async def stop_datea(event):
     global zedself
     if zedself:
         zedself = False
-        return await edit_or_reply(event, "**𓆰تم تعطيـل حفظ الذاتيـة التلقائـي .. بنجـاح ☑️**")
-    await edit_or_reply(event, "**𓆰حفظ الذاتيـة التلقـائي .. معطـلة مسـبـقًا ☑️**")
+        return await edit_or_reply(event, "**⎉╎تم تعطيـل حفظ الذاتيـة التلقائـي .. بنجـاح ☑️**")
+    await edit_or_reply(event, "**⎉╎حفظ الذاتيـة التلقـائي .. معطلـه مسبقـاً ☑️**")
 
-#code for @R0R77
 @zedub.on(events.NewMessage(func=lambda e: e.is_private and (e.photo or e.video) and e.media_unread))
 async def sddm(event):
     global zedself
@@ -71,44 +73,44 @@ async def sddm(event):
         sender = await event.get_sender()
         chat = await event.get_chat()
         pic = await event.download_media()
-        await zedub.send_file("me", pic, caption=f"[ᯓ 𝗦𝗼𝘂𝗿𝗰𝗲 𝐑𝐄𝐅𝐙 - حفـظ الذاتـيـة ♥️](t.me/def_Zoka) .\n\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\n**⌔╎مࢪحبـاً عـزيـزي المـالك 🫂\n⌔╎ تـم حفـظ الذاتيـة تلقائيـاً .. بنجـاح ☑️** ❝\n**⌔╎المـرسـل** {_format.mentionuser(sender.first_name , sender.id)} .")
+        await zedub.send_file("me", pic, caption=f"[ᯓ 𝗦𝗼𝘂𝗿𝗰𝗲 𝐑𝐄𝐅𝐙 - حفـظ الذاتيـه 🧧](t.me/def_Zoka) .\n\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\n**⌔╎مࢪحبـاً عـزيـزي المـالك 🫂\n⌔╎ تـم حفـظ الذاتيـة تلقائيـاً .. بنجـاح ☑️** ❝\n**⌔╎المـرسـل** {_format.mentionuser(sender.first_name , sender.id)} .")
 
-@zedub.zed_cmd(
-    pattern="تست (\d*) ([\s\S]*)",
-    command=("sdm", plugin_category),
-    info={
-        "header": "To self destruct the message after paticualr time.",
-        "الاسـتخـدام": "{tr}sdm [number] [text]",
-        "مثــال": "{tr}sdm 10 hi",
-    },
-)
+#Code For T.me/zzzzl1l
+@zedub.zed_cmd(pattern="اعلان (\d*) ([\s\S]*)")
 async def selfdestruct(destroy):
-    "To self destruct the sent message"
-    cat = ("".join(destroy.text.split(maxsplit=1)[1:])).split(" ", 1)
-    message = cat[1]
-    ttl = int(cat[0])
+    zed = ("".join(destroy.text.split(maxsplit=1)[1:])).split(" ", 1)
+    message = zed[1]
+    ttl = int(zed[0])
+    zelzal = ttl * 60 #تعييـن الوقـت بالدقائـق بدلاً من الثـوانـي
     await destroy.delete()
     smsg = await destroy.client.send_message(destroy.chat_id, message)
-    await sleep(ttl)
+    await sleep(zelzal)
     await smsg.delete()
 
-@zedub.zed_cmd(
-    pattern="محترقه (\d*) ([\s\S]*)",
-    command=("selfdm", plugin_category),
-    info={
-        "header": "To self destruct the message after paticualr time. and in message will show the time.",
-        "الاسـتخـدام": "{tr}selfdm [number] [text]",
-        "مثــال": "{tr}selfdm 10 hi",
-    },
-)
+#Code For T.me/zzzzl1l
+@zedub.zed_cmd(pattern="إعلان (\d*) ([\s\S]*)")
 async def selfdestruct(destroy):
-    "To self destruct the sent message"
-    cat = ("".join(destroy.text.split(maxsplit=1)[1:])).split(" ", 1)
-    message = cat[1]
-    ttl = int(cat[0])
-    text = message + f"\n\n`This message shall be self-destructed in {ttl} seconds`"
-
+    zed = ("".join(destroy.text.split(maxsplit=1)[1:])).split(" ", 1)
+    message = zed[1]
+    ttl = int(zed[0])
+    zelzal = ttl * 60 #تعييـن الوقـت بالدقائـق بدلاً من الثـوانـي
+    text = message + f"\n\n**- هذا الاعلان سيتم حذفه تلقـائيـاً بعـد {zelzal} دقائـق ⏳**"
     await destroy.delete()
     smsg = await destroy.client.send_message(destroy.chat_id, text)
-    await sleep(ttl)
+    await sleep(zelzal)
     await smsg.delete()
+
+
+@zedub.on(events.NewMessage(incoming=True))
+async def gpost(event):
+    if event.is_private:
+        return
+    chat_id = str(event.chat_id).replace("-100", "")
+    channels_set  = get_all_post(chat_id)
+    if channels_set == []:
+        return
+    for chat in channels_set:
+        if event.media:
+            await event.client.send_file(int(chat), event.media, caption=event.text)
+        elif not event.media:
+            await zedub.send_message(int(chat), event.message)
