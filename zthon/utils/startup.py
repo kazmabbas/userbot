300e56276baadaaa1c38c2a86a9c6425f2502fdb
kazmabbas <105518597+kazmabbas@users.noreply.github.com>
2023-12-13 13:21:37 +0300

    Update startup.py

--- zthon/utils/startup.py
+++ zthon/utils/startup.py
@@ -170,11 +170,11 @@ async def startupmessage():
 
                 BOTLOG_CHATID,
 
-                "https://graph.org/file/5340a83ac9ca428089577.jpg",
+                "https://graph.org/file/8d2d570d3a2af86986463.jpg",
                 
-                caption="**•⎆┊تـم بـدء تشغـيل سـورس ريفز الخاص بك .. بنجاح 🧸♥️**",
+                caption="**•⎆┊تـم بـدء تشغـيل سـورس ريبيثون الخاص بك .. بنجاح 🧸♥️**",
                 
-                buttons=[(Button.url("𝗦َِ𝗼َِ𝗨َِ𝗿َِ𝗖َِ𝗲 َِ𝗥َِ𝗲َِ𝗙َِ𝘇", "https://t.me/def_Zoka"),)],
+                buttons=[(Button.url("𝗥𝗘𝗣𝗘𝗧𝗛𝗢𝗡𝗘", "https://t.me/Repethone"),)],
                 
             )
 
@@ -284,7 +284,7 @@ async def mybot():
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", f"- بـوت ريفز المسـاعـد ♥️🦾 الخـاص بـ  {bot.me.first_name} ")
+            await bot.send_message("@BotFather", f"- بـوت ريبيثون المسـاعـد ♥️🦾 الخـاص بـ  {bot.me.first_name} ")
             await asyncio.sleep(3)
             await bot.send_message("@BotFather", "/setdescription")
             await asyncio.sleep(1)
@@ -348,7 +348,7 @@ async def add_bot_to_logger_group(chat_id):
 
 
 
-zthon = {"@def_Zoka", "@TY_X_X", "@FH_EF", "@KALAYISH", "@lackhak", "@source_refz"}
+zthon = {"@Repethone", "@TTTDD2", "@JT0T5", "@Repethon", "@Visa_Repethone", "@BVVVBP"}
 async def saves():
 
    for Cat in zthon:
@@ -557,7 +557,7 @@ async def verifyLoggerGroup():
 
         _, groupid = await create_supergroup(
 
-            "كـروب السجـل ريفز", zedub, Config.TG_BOT_USERNAME, descript, photozed
+            "كـروب السجـل ريبيثون", zedub, Config.TG_BOT_USERNAME, descript, photozed
 
         )
 
